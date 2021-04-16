1cb7d06ac3450c326321da03dc4da4a6e0ec92d7
sam <sblan45@gmail.com>
2021-04-15 22:54:50 -0400

    adds for loop block

--- program.cs
+++ program.cs
@@ -1,27 +1,37 @@
 ﻿using System;
 using static System.Console;
 
-namespace ControlFlow
+public class Program
 {
-  class Program
+  public static void Main()
   {
-    static void Main(string[] args)
+    /*  create an array of integer-based test scores */
+    /*  NOTE:  array indices start with zero */
+    int[] Scores = new int[] { 105, 92, 81, 93, 100 };
+    int MyScore;
+
+    /* loop through the following loop 5 times - once for each element of our Scores array */
+    for (int i = 4; i >= 0; i--)
     {
 
-      Random r = new Random();
-      int newRandom, counter = 0;
+      /*  assign the MyScore variable to the i'th element of the Scores array */
+      MyScore = Scores[i];
 
-      while (counter < 5)
+      switch (MyScore)
       {
-        /* get a random number between 0 and 99 inclusive */
-        newRandom = r.Next(0, 100);
-
-        WriteLine("Random number " + counter + " was " + newRandom);
-
-        /* increment our counter */
-        counter += 1;
+        case 105:
+          WriteLine("Our test score was 105.");
+          break;
+        case 92:
+          WriteLine("Our test score was 92.");
+          break;
+        case 81:
+          WriteLine("Our test score was 81.");
+          break;
+        default:
+          WriteLine("Our test score was not 105, 92 or 81.");
+          break;
       }
-      ReadLine();
     }
   }
 }
