f9b742a6ab6744a4c9f51fac873925133abae7b9
sam <sblan45@gmail.com>
2021-04-16 19:03:36 -0400

    adds two dimentional arrays

--- program.cs
+++ program.cs
@@ -5,32 +5,16 @@ public class Program
 {
   public static void Main()
   {
-    /*  create an array of integer-based test scores */
-    /*  NOTE:  array indices start with zero */
-    int[] Scores = new int[] { 105, 92, 81, 93, 100 };
-    int MyScore;
+    int[,] twodim = new int[,] { { 1, 2, 3 }, { 4, 5, 6 } };
 
-    /* loop through the following loop 5 times - once for each element of our Scores array */
-    for (int i = 4; i >= 0; i--)
+    int dim1 = twodim.GetLength(0);
+    int dim2 = twodim.GetLength(1);
+
+    for (int i = 0; i < dim1; i++)
     {
-
-      /*  assign the MyScore variable to the i'th element of the Scores array */
-      MyScore = Scores[i];
-
-      switch (MyScore)
+      for (int j = 0; j < dim2; j++)
       {
-        case 105:
-          WriteLine("Our test score was 105.");
-          break;
-        case 92:
-          WriteLine("Our test score was 92.");
-          break;
-        case 81:
-          WriteLine("Our test score was 81.");
-          break;
-        default:
-          WriteLine("Our test score was not 105, 92 or 81.");
-          break;
+        WriteLine("twodim[{0}, {1}] = {2}", i, j, twodim[i, j]);
       }
     }
   }
